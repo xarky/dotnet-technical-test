5f6629d77aa0bfc475359d9b3530eb7c513cde4a
bpace <bernard.pace@kpmg.co.uk>
2019-08-24 16:57:58 +0200

    Added unit tests for managers

---
+++ UnitTests/BusinessLogic/AccountsManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using POC.BusinessLogic.Managers;
+using POC.Common;
+using System;
+using Xunit;
+
+namespace UnitTests.BusinessLogic
+{
+    public class AccountsManagerTest
+    {
+        #region Fields
+
+        private readonly Mock<IRepository> repository;
+
+        #endregion
+
+        #region Constructors
+
+        public AccountsManagerTest()
+        {
+            repository = new Mock<IRepository>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Theory]
+        [InlineData(1, 100d)]
+        [InlineData(2, 0d)]
+        [InlineData(3, 50.2d)]
+        [InlineData(4, 45.1d)]
+        public void GetAvailableFunds_Success(Int32 customerId, Decimal funds)
+        {
+            repository
+                .Setup(a => a.GetAvailableFunds(customerId))
+                .Returns(funds);
+
+            var accountsManager = new AccountsManager(repository.Object);
+
+            var availableFunds = accountsManager.GetAvailableFunds(customerId);
+
+            Assert.Equal(funds, availableFunds);
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/BusinessLogic/CustomersManagerTest.cs
@@ -0,0 +1,107 @@
+﻿using FizzWare.NBuilder;
+using Moq;
+using POC.BusinessLogic.Managers;
+using POC.Common;
+using POC.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace UnitTests.BusinessLogic
+{
+    public class CustomersManagerTest
+    {
+        #region Fields
+
+        private readonly Mock<IRepository> repository;
+
+        #endregion
+
+        #region Constructors
+
+        public CustomersManagerTest()
+        {
+            repository = new Mock<IRepository>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(100)]
+        [InlineData(1000)]
+        public void GetCustomers_CustomerExist(Int32 customerCount)
+        {
+            var testData = Builder<Customer>.CreateListOfSize(customerCount).Build();
+
+            repository
+                .Setup(a => a.GetCustomers())
+                .Returns(testData);
+
+            var customersManager = new CustomersManager(repository.Object);
+
+            var result = customersManager.GetCustomers();
+
+            Assert.Equal(customerCount, result.Count());
+        }
+
+        [Fact]
+        public void GetCustomers_NoData()
+        {
+            repository
+                  .Setup(a => a.GetCustomers())
+                  .Returns(new List<Customer>());
+
+            var customersManager = new CustomersManager(repository.Object);
+
+            var result = customersManager.GetCustomers();
+
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void GetCustomer_Success(Int32 customerId)
+        {
+            var testData = Builder<Customer>.CreateNew().Build();
+
+            repository
+               .Setup(a => a.GetCustomer(customerId))
+               .Returns(testData);
+
+            var customersManager = new CustomersManager(repository.Object);
+
+            var result = customersManager.GetCustomer(customerId);
+
+            var obj1Str = JsonConvert.SerializeObject(testData);
+            var obj2Str = JsonConvert.SerializeObject(result);
+
+            Assert.Equal(obj1Str, obj1Str);
+        }
+
+        [Fact]
+        public void GetCustomer_NotExisting()
+        {
+            repository
+               .Setup(a => a.GetCustomer(-1))
+               .Returns((Customer)null);
+
+            var customersManager = new CustomersManager(repository.Object);
+
+            var result = customersManager.GetCustomer(-1);
+            
+            Assert.Null(result);
+        }
+
+        #endregion
+    }
+}
